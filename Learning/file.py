2b62e6f8a7d8d5a8cdc0f00513daaa0a72bda13d
StarGazer1995 <gz19951230@outlook.com>
2019-03-22 21:29:15 +0800

    learning

--- Learning/file.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import os
-import tensorflow as tf
-from tensorflow import keras
-import pandas as pd
-
-
-filename=os.path.abspath("G:\\Kaggle\\Data Set\\all\\train_labels.csv")
-data=pd.read_csv(filename)
